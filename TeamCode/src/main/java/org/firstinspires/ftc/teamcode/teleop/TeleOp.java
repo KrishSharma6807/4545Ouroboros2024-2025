d7a9798e43a3152d07d97c6ce900974ff6a6c1ab
metajet <phoenixheath@gmail.com>
2024-09-11 16:25:31 -0500

    added methods to teleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOp.java
@@ -3,6 +3,14 @@ package org.firstinspires.ftc.teamcode.teleop;
 public class TeleOp extends TeleLib{
     @Override
     public void loop(){
+        ArcadeDrive();
+        horizSlides();
+        intake();
+        Claw();
+    }
 
+    @Override
+    public void kill(){
+        kill();
     }
 }
