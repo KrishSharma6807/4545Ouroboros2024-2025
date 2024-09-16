045c8d2fe638a2abe37c6eba3d55f735dfe6f4c1
SatvikVasikarla <satvik.vasikarla@gmail.com>
2024-09-16 16:29:48 -0500

    Practice Auto Basic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/practiceAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/practiceAuto.java
@@ -4,24 +4,37 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
+
 
 public abstract class practiceAuto extends LinearOpMode {
+
     public DcMotor br;
+
     public DcMotor bl;
+
     public DcMotor fr;
+
     public DcMotor fl;
 
-    br = hardwareMap.get(DcMotor.class, "br");
-    fr = hardwareMap.get(DcMotor.class, "fr");
-    fl = hardwareMap.get(DcMotor.class, "fl");
-    bl = hardwareMap.get(DcMotor.class, "bl");
-
     @Override
+    public void runOpMode() throws InterruptedException {
+
+        br = hardwareMap.get(DcMotor.class, "br");
+        fr = hardwareMap.get(DcMotor.class, "fr");
+        fl = hardwareMap.get(DcMotor.class, "fl");
+        bl = hardwareMap.get(DcMotor.class, "bl");
+
+        br.setDirection(DcMotorSimple.Direction.REVERSE);
+        bl.setDirection(DcMotorSimple.Direction.FORWARD);
+        fr.setDirection(DcMotorSimple.Direction.REVERSE);
+        fl.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        waitForStart();
+        resetRuntime();
+
+        }
+    }
 }
