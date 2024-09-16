153a61ee02d6b01e589740e621bb0cd557335e15
Etienne <etienne.c.lambert@gmail.com>
2024-09-16 16:15:37 -0500

    gig

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/practiceAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/practiceAuto.java
@@ -6,10 +6,22 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
-
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public abstract class practiceAuto extends LinearOpMode {
+    public DcMotor br;
+    public DcMotor bl;
+    public DcMotor fr;
+    public DcMotor fl;
 
+    br = hardwareMap.get(DcMotor.class, "br");
+    fr = hardwareMap.get(DcMotor.class, "fr");
+    fl = hardwareMap.get(DcMotor.class, "fl");
+    bl = hardwareMap.get(DcMotor.class, "bl");
 
-
+    @Override
 }
