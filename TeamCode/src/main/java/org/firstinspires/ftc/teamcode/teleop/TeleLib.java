76d4cc5ed1458562c6057907084c29ddba9e1cef
DarkSkyVoid <139508016+DarkSkyVoid@users.noreply.github.com>
2024-11-19 18:10:16 -0600

    Made my Actions for the Intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleLib.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleLib.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-import com.qualcomm.robotcore.hardware.Servo
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
